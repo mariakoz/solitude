11ae337adc146fffa6ac47d59168ed3c910e32f2
alvertosk <alvertosk@yahoo.com>
2016-08-01 21:45:14 +0300

    Add files via upload

---
+++ heartbeats.ino
@@ -0,0 +1,41 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  
+}
+// the loop function runs over and over again forever
+void loop() {
+
+//fast heartbeat
+ //digitalWrite(12, HIGH);   // turn the Vibration Motor on (HIGH is the voltage level)
+ // delay(100);              // wait for a second
+ // digitalWrite(12, LOW);    // turn the Vibration Motor off by making the voltage LOW
+ // delay(50);
+ //  digitalWrite(12, HIGH);   // turn the Vibration Motor on (HIGH is the voltage level)
+ // delay(100);              // wait for a second
+ // digitalWrite(12, LOW);    // turn the Vibration Motor off by making the voltage LOW
+ // delay(200);
+
+
+//normal heartbeat
+//digitalWrite(12, HIGH);   // turn the Vibration Motor on (HIGH is the voltage level)
+//  delay(200);              // wait for a second
+//  digitalWrite(12, LOW);    // turn the Vibration Motor off by making the voltage LOW
+//  delay(100);
+//   digitalWrite(12, HIGH);   // turn the Vibration Motor on (HIGH is the voltage level)
+//  delay(200);              // wait for a second
+//  digitalWrite(12, LOW);    // turn the Vibration Motor off by making the voltage LOW
+//  delay(500);
+
+//slow heartbeat
+//digitalWrite(12, HIGH);   // turn the Vibration Motor on (HIGH is the voltage level)
+//  delay(300);              // wait for a second
+ // digitalWrite(12, LOW);    // turn the Vibration Motor off by making the voltage LOW
+ // delay(200);
+ //  digitalWrite(12, HIGH);   // turn the Vibration Motor on (HIGH is the voltage level)
+  //delay(300);              // wait for a second
+  //digitalWrite(12, LOW);    // turn the Vibration Motor off by making the voltage LOW
+  //delay(800);
+}
